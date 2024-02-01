ecb9e632c920d038f50b5a0951fba105468074c1
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:13:11 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,26 +10,26 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (!parent)
-        return (NULL);
+	/* Check if parent is NULL */
+	if (!parent)
+		return (NULL);
 
-    /* Create a new node with the given value */
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
+	/* Create a new node with the given value */
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
-    /* If parent already has a left-child, adjust pointers */
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
+	/* If parent already has a left-child, adjust pointers */
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
-    /* Set the new node as the left-child of the parent */
-    parent->left = new_node;
+	/* Set the new node as the left-child of the parent */
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
