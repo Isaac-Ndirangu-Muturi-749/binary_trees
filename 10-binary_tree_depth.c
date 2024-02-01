2b7f4060889d15da6ca4c31a0237b3f6c4e89943
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:37:20 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,18 +9,18 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    /* Check if tree is NULL */
-    if (!tree)
-        return (0);
+	/* Check if tree is NULL */
+	if (!tree)
+		return (0);
 
-    /* Recursively calculate the depth until the root is reached */
-    while (tree->parent)
-    {
-        tree = tree->parent;
-        depth++;
-    }
+	/* Recursively calculate the depth until the root is reached */
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		depth++;
+	}
 
-    return (depth);
+	return (depth);
 }
