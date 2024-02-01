1b0759a2e73dea46d2bda0b602ea8c79ed0fb023
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:45:49 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,16 +9,16 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height == right_height)
-        return (1);
+	if (left_height == right_height)
+		return (1);
 
-    return (0);
+	return (0);
 }
