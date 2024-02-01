a2b3b6f6980c4adf7e2edef2df36a1c6b01722e3
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:45:55 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,16 +10,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left && node->parent->right)
-    {
-        if (node->parent->left == node)
-            return (node->parent->right);
-        else
-            return (node->parent->left);
-    }
+	if (node->parent->left && node->parent->right)
+	{
+		if (node->parent->left == node)
+			return (node->parent->right);
+		else
+			return (node->parent->left);
+	}
 
-    return (NULL);
+	return (NULL);
 }
