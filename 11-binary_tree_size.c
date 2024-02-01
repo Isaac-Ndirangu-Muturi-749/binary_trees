46bcb3743be4df4d2e3a2c73ce378a7801eb5a21
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:42:12 +0300

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,8 +15,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	/** Recursively calculate the size by adding the sizes of the left and
-	 * right subtrees */
+	/* Recursively calculate the size by adding the sizes of the left and*/
+	 /* right subtrees */
 	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
 	return (size);
