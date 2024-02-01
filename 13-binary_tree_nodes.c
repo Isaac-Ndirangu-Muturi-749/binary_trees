b6ae0e033520dece2705de7d0ff890d6c14db4d8
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-01 21:43:32 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,18 +9,18 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    /* Check if tree is NULL */
-    if (!tree)
-        return (0);
+	/* Check if tree is NULL */
+	if (!tree)
+		return (0);
 
-    /* Check if the current node has at least 1 child */
-    if (tree->left || tree->right)
-        nodes++;
+	/* Check if the current node has at least 1 child */
+	if (tree->left || tree->right)
+		nodes++;
 
-    /* Recursively count nodes in the left and right subtrees */
-    nodes += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	/* Recursively count nodes in the left and right subtrees */
+	nodes += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 
-    return (nodes);
+	return (nodes);
 }
